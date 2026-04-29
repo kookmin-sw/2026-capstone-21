1b6dee7b7c36c383b7118a05f1226f62f61a8fe5
yunji0417 <cyjtwin@naver.com>
2026-04-29 18:15:31 +0900

    seed log 데이터 view->detail_view

--- app/seed/seed_logs.py
+++ app/seed/seed_logs.py
@@ -5,7 +5,7 @@ def seed_logs():
     db = SessionLocal()
     # 수동으로 넣고 싶은 로그 데이터
     sample_logs = [
-        {"user_id": 1, "influencer_id": 10, "action_type": "view", "reward": 1},
+        {"user_id": 1, "influencer_id": 10, "action_type": "detail_view", "reward": 1},
         {"user_id": 1, "influencer_id": 12, "action_type": "contact", "reward": 3},
     ]
     for log in sample_logs:
