f198b9dcbc6c188f96508392f67752d40b8e1af0
lovesuperlit <134582755+lovesuperlit@users.noreply.github.com>
2026-05-01 15:34:18 +0900

    5월1일수정_챗웃, 추천 등에 일단 주석처리함
    Comment out unused router imports and includes.

--- app/main.py
+++ app/main.py
@@ -6,13 +6,13 @@ from app.routers import (
     category,
     influencer,
     mall_input,
-    recommendation,
-    user_action_log,
+    # recommendation,
+    # user_action_log,
     favorite,
     insight,
-    admin,
+    # admin,
     auth,
-    chatwoot
+    # chatwoot
 )
 
 app = FastAPI(root_path="/proxy/8000")
@@ -32,12 +32,12 @@ app.include_router(category.router)
 app.include_router(auth.router)
 app.include_router(influencer.router)
 app.include_router(mall_input.router)
-app.include_router(recommendation.router)
-app.include_router(user_action_log.router)
+# app.include_router(recommendation.router)
+# app.include_router(user_action_log.router)
 app.include_router(favorite.router)
 app.include_router(insight.router)
-app.include_router(admin.router)
-app.include_router(chatwoot.router)
+# app.include_router(admin.router)
+# app.include_router(chatwoot.router)
 
 
 @app.get("/")
