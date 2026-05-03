19c09c2e0b72e4cc53be67fab83fbd8d96f28182
yunji0417 <cyjtwin@naver.com>
2026-05-03 12:37:49 +0900

    Insights Selection 지표를 실제 매칭 완료(collaboration_complete) 기준으로 수정

--- app/crud/insight.py
+++ app/crud/insight.py
@@ -10,8 +10,8 @@ from app.db.models import (
     InfluencerCategory,
 )
 
-
-SELECTION_ACTION = "favorite_add"
+# 인플루언서와 실제 매칭 완료됐을 때 카운트
+SELECTION_ACTION = "collaboration_complete"
 
 
 def get_total_selections(db: Session):
