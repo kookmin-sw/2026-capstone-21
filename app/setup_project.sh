aadbac191a695cc03fe0c61b1c0883f17d39a961
eunjin <20232861@kookmin.ac.kr>
2026-05-07 00:39:27 +0000

    tnstj

--- app/setup_project.sh
+++ app/setup_project.sh
@@ -10,11 +10,11 @@ python -m app.seed.seed_categories
 echo "📸 [4/7] 인플루언서 데이터 시딩..."
 python -m app.seed.seed_influencers
 
-echo "📝 [5/7] 초기 액션 로그 데이터 시딩..."
-python -m app.seed.seed_logs
-
-echo "🤷‍♀️ [6/7] 초기 유저 데이터 시딩..."
+echo "🤷‍♀️ [5/7] 초기 유저 데이터 시딩..."
 python -m app.seed.seed_users
+
+echo "📝 [6/7] 초기 액션 로그 데이터 시딩..."
+python -m app.seed.seed_logs
 
 echo "🧠 [7/7] FAISS 벡터 임베딩 구축..."
 python -m app.services.build_influencer_embeddings
