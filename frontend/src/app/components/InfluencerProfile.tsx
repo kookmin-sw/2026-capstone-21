c598e52efe98c819abae08655874089a85c24eb5
eunjin <20232861@kookmin.ac.kr>
2026-05-09 08:47:59 +0000

    ㅅㅈ

--- frontend/src/app/components/InfluencerProfile.tsx
+++ frontend/src/app/components/InfluencerProfile.tsx
@@ -530,7 +530,7 @@ export function InfluencerProfile() {
       </AnimatePresence>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8">
-        {filteredInfluencers.map((influencer) =>
+        {displayedInfluencers.map((influencer) =>
           renderInfluencerCard(influencer)
         )}
       </div>
